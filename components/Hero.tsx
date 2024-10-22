cf2890628e4b0de92ffd1c05a3cb91511e524b33
Caleb Bae <baecal000@gmail.com>
2024-10-21 19:03:36 -0500

    switch checkbox side

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,19 +24,20 @@ const Hero = () => {
                 <span className="font-bold">A</span>pplication
               </h1>
             </div>
-            <Badge className="mt-5 w-1/2 mx-auto flex items-center justify-center py-2 bg-white text-black rounded-3xl space-x-3 text-md hover:bg-white font-normal">
+            <Badge className="mt-5 w-1/2 mx-auto flex items-center justify-center py-2 bg-white text-black rounded-3xl space-x-3 text-md hover:bg-white">
+              <FaCheck className="text-green-500 " />
               <div className="flex items-center justify-center">
                 Notifications
               </div>
-              <FaCheck className="text-green-500" />
+              <FaCheck className=" text-green-500 " />
+
               <div className="flex items-center justify-center">
                 AI Schedule Creation
               </div>
-              <FaCheck className="text-green-500" />
+              <FaCheck className="text-green-500 " />
               <div className="flex items-center justify-center space-x-2">
                 Simplicity
               </div>
-              <FaCheck className="text-green-500" />
             </Badge>
           </>
         }
